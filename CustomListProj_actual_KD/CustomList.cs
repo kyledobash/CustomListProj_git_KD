2e50d1967ae05ecec02632adde433792710c257b
kyledobash <kyledobash@yahoo.com>
2021-01-04 11:03:07 -0700

    Added the CustomListTests project, and linked with CustomListProj

---
+++ CustomListProj_actual_KD/CustomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProj_actual_KD
+{
+    public class CustomList
+    {
+        // member variables
+        
+
+
+        // constructor
+        public CustomList()
+        {
+
+        }
+
+        // member methods
+
+    }
+}
---
+++ CustomListTests/AddTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class AddTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
