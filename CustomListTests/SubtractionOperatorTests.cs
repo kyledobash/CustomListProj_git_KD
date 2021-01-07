22e85668f8bb97dd3ec22a5db77137ce1eb04c05
kyledobash <kyledobash@yahoo.com>
2021-01-07 14:49:28 -0700

    Created skeleton for the subraction operator overload method. Also created SubtractionOperatorTests and created 2 test methods within it.

---
+++ CustomListTests/SubtractionOperatorTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListProj_actual_KD;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class SubtractionOperatorTests
+    {
+        [TestMethod]
+        public void SubtractionOperator_TwoSingleItemLists_ResultListBecomesEmpty()
+        {
+            // arrange
+            CustomList<int> testList1 = new CustomList<int>();
+            CustomList<int> testList2 = new CustomList<int>();
+            CustomList<int> resultList = new CustomList<int>();
+
+            testList1.Add(1);
+            testList2.Add(1);
+
+            int expected = 0;
+            int actual;
+
+            // act
+            resultList = testList1 - testList2;
+            actual = resultList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SubtractionOperator_TwoListsShareNoSameValues_ListRemainsTheSame()
+        {
+            // arrange
+            CustomList<int> testList1 = new CustomList<int>();
+            CustomList<int> testList2 = new CustomList<int>();
+            CustomList<int> resultList = new CustomList<int>();
+
+            testList1.Add(1);
+            testList2.Add(2);
+
+            int expected = 1;
+            int actual;
+
+            // act
+            resultList = testList1 - testList2;
+            actual = resultList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
